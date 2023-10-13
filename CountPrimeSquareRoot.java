e51b39d555f0344331a11f743de6f9613f8fddc0
abhi-ksh <40681397+abhi-ksh@users.noreply.github.com>
2023-10-13 12:04:08 -0400

    Create CountPrimeSquareRoot.java

---
+++ CountPrimeSquareRoot.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countPrimes(int n) {
+        
+        //Simple implementation of Eratorsthenes sieve
+        
+        if (n < 2){
+            return 0;
+        }
+
+        int primes = 0 ;       
+        boolean [] grid = new boolean[n]; //default value false
+        
+        //flagging loop
+        for (int i = 2; i*i < n ; i++){
+            
+            // 2 is the first prime.. then mark everything in multiple of 2 as true            
+            if(!grid[i]){                
+                // numbers greather than the square and multiple of the number being tested should be flagged 
+                for (int j = i*i; j < n; j = j+i){
+                    // System.out.println("j:" + j);
+                    grid[j] = true;
+                }
+            }        
+        }
+
+        for (int i = 2; i < n; i++){
+            if(!grid[i]){
+                primes++;
+            }
+        }
+        
+        return primes;
+    }
+}
